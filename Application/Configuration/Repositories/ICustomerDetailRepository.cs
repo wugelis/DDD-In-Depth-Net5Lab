1b984a4d12dafaeb4bb7282324bf757a3ef49255
wugelis <wergelis@msn.com>
2021-05-10 18:32:58 +0800

    改變 repositories 位置在 Application Layer/Use Case Layer, 修正 DetailValue 的位置為 Domain Layer

---
+++ Application/Configuration/Repositories/ICustomerDetailRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomerDomain = global::Domain.Customer;
+
+namespace Application.Configuration.Repositories
+{
+    public interface ICustomerDetailRepository
+    {
+        IEnumerable<CustomerDomain.Customer> GetCustomers();
+        int AddBasicCustomerDetailData(CustomerDomain.Customer customer);
+    }
+}
---
+++ Domain/Customer/DetailData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Customer
+{
+    public class DetailData : ValueObject
+    {
+        private string _myLocaton;
+        public DetailData(string location)
+        {
+            _myLocaton = location;
+        }
+        public string Location 
+        { 
+            get
+            {
+                return _myLocaton;
+            }
+            set
+            {
+                _myLocaton = value;
+            }
+        }
+    }
+}
