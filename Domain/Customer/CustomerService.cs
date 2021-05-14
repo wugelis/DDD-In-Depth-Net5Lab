7ca4203023b02fdd2aee37b13fc932c8cf034223
wugelis <wergelis@msn.com>
2021-05-14 15:04:20 +0800

    在Repository增加Get(), 移動DetailData.cs到Domain,Customer 的 Entity 增加 Location 屬性 與 增加 檢查（帳號／身分證號）是否為特殊身分的領域邏輯方法

---
+++ Domain/Customer/CustomerService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Customer
+{
+    public class CustomerService
+    {
+        public CustomerService()
+        {
+        }
+
+        /// <summary>
+        /// 檢查是否為特出身分使用者
+        /// 可能呼叫機關提供之服務以檢查是否為特殊身分國民
+        /// </summary>
+        /// <param name="customer">客戶物件實體</param>
+        /// <param name="accountId">可能是身份證字號</param>
+        /// <returns></returns>
+        public void CheckIsSpecialAccount(Customer customer, string accountId)
+        {
+            bool isSpecial = customer.CheckSpecialUser(accountId);
+
+            if (isSpecial)
+            {
+                customer.ShowSpecialFlag();
+            }
+        }
+    }
+}
